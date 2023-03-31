fe90bb406dc95f483a69a36370503c3b35972a7d
MarianoIsmael <110506444+MarianoIsmael@users.noreply.github.com>
2023-03-30 21:13:09 -0300

    Create Jugador.ts

---
+++ Jugador.ts
@@ -0,0 +1,10 @@
+import { MiembroClub } from './AbstractMiembroClub';
+
+export enum Deporte {
+  Futbol = 'futbol',
+  Basket = 'basket',
+  Zumba = 'zumba',
+  Voley = 'voley',
+  Natacion = 'natacion',
+  Gym = 'gym',
+}
